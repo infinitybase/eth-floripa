0da616ef9e4e5a4430e55655063529815653410b
vitor <pereiravitor1218@gmail.com>
2025-08-26 16:56:46 -0300

    feat(metadata): update site metadata and add icons for better SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,13 +20,40 @@ export const metadata: Metadata = {
   title: 'ETH Floripa November 11 - 12 in Florianópolis, Brazil',
   description:
     'ETH Floripa is a curated, off-the-record, retreat between ETH LATAM and Devconnect designed for ~150 top global builders, founders, and researchers November 11-12, 2025 in Florianópolis, Brazil.',
+  robots: "index, follow",
+  icons: [
+    {
+      url: '/favicon.ico',
+      type: 'image/x-icon',
+      rel: 'shortcut icon',
+    },
+    {
+      url: '/apple-touch-icon.png',
+      type: 'image/png',
+      sizes: '180x180',
+      rel: 'apple-touch-icon',
+    },
+    {
+      url: '/android-chrome-192x192.png',
+      type: 'image/png',
+      sizes: '192x192',
+      rel: 'icon',
+    },
+    {
+      url: '/android-chrome-512x512.png',
+      type: 'image/png',
+      sizes: '512x512',
+      rel: 'icon',
+    },
+  ],
+  manifest: "/site.webmanifest",
   openGraph: {
     title: 'ETH Floripa November 11 - 12 in Florianópolis, Brazil',
     description:
       'ETH Floripa is a curated, off-the-record, retreat between ETH LATAM and Devconnect designed for ~150 top global builders, founders, and researchers. November 11-12, 2025 in Florianópolis, Brazil.',
     type: 'website',
     siteName: 'ETH Floripa',
-    url: 'https://lu.ma/m5hujc9a',
+    url: 'https://www.ethfloripa.com/',
     countryName: 'Brazil',
   },
 };
@@ -37,7 +64,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt">
+    <html lang="pt-br">
+      <head>
+        {/* Google Fonts */}
+         <link rel="preconnect" href="https://fonts.googleapis.com" />
+      </head>
       <body className={`${interSans.variable} ${bricolage.variable}`}>
         <Header />
         {children}
